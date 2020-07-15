751cf0e6512c7e2ed7a81b0f8e6d18823a282c62
jacalarcon <jacalarcon@ingenia.es>
2020-07-15 13:22:38 +0200

    Nuevo texto en el bloque

--- block_superframe.php
+++ block_superframe.php
@@ -77,7 +77,7 @@ class block_superframe extends block_base {
         $this->content = new stdClass();
         $this->content->footer = '';
         $this->content->text = get_string('welcomeuser', 'block_superframe',
-                $USER);
+                $USER) . "<br />" . get_string('message', 'block_superframe');
 
         return $this->content;
     }
