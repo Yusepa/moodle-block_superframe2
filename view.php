38e2ca59a46a1cc972b3ed412fd2546819938d63
jacalarcon <jacalarcon@ingenia.es>
2020-08-04 07:51:16 +0200

    Add renderer and mustache template

--- view.php
+++ view.php
@@ -49,15 +49,6 @@ if ($configdata) {
     $config->size = 'custom';
 }
 
-// Start output to browser.
-echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('pluginname', 'block_superframe'), 5);
-
-// Content.
-echo '<br>' . fullname($USER) . '<br>';
-echo $OUTPUT->user_picture($USER);
-
-echo '<br><br>';
 
 // URL - comes either from instance or admin.
 $url = $config->url;
@@ -81,12 +72,5 @@ switch ($config->size) {
         break;
 }
 
-// Build and display an iframe.
-$attributes = ['src' => $url,
-               'width' => $width,
-               'height' => $height];
-echo html_writer::start_tag('iframe', $attributes);
-echo html_writer::end_tag('iframe');
-
-// Send footer out to browser.
-echo $OUTPUT->footer();
+$renderer = $PAGE->get_renderer('block_superframe');
+$renderer->display_view_page($url, $width, $height);
